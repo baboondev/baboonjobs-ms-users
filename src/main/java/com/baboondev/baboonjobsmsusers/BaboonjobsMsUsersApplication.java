d39abe4ea5b72e4919037dd554ddedeb551ffc58
msosa <marcossosa011@gmail.com>
2021-10-25 20:38:42 -0300

    Initialize spring

---
+++ src/main/java/com/baboondev/baboonjobsmsusers/BaboonjobsMsUsersApplication.java
@@ -0,0 +1,13 @@
+package com.baboondev.baboonjobsmsusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaboonjobsMsUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaboonjobsMsUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baboondev/baboonjobsmsusers/BaboonjobsMsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baboondev.baboonjobsmsusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaboonjobsMsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
