eecb74b3881d8ba06e35d236e93fa33d529b44d0
msosa <marcossosa011@gmail.com>
2021-11-27 16:50:53 -0300

    bugfix: config cors

--- src/main/java/com/baboondev/baboonjobsmsusers/controllers/UserController.java
+++ src/main/java/com/baboondev/baboonjobsmsusers/controllers/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins="*", methods = {RequestMethod.POST})
 @RequestMapping("auth")
 public class UserController {
     private final Logger logger = LoggerFactory.getLogger(UserController.class);
