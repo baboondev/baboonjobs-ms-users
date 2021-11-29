625d3919472e85da08cf6400b77a1820dde1c0f9
Nicolas Caro <niicolas.caro@gmail.com>
2021-11-29 00:55:03 -0300

    Add swagger-ui and change default port

--- src/main/java/com/baboondev/baboonjobsmsusers/BaboonjobsMsUsersApplication.java
+++ src/main/java/com/baboondev/baboonjobsmsusers/BaboonjobsMsUsersApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BaboonjobsMsUsersApplication {
@@ -15,5 +19,14 @@ public class BaboonjobsMsUsersApplication {
     @Bean
     public BCryptPasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public Docket usersApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.baboondev.baboonjobsmsusers"))
+                .paths(PathSelectors.any())
+                .build();
     }
 }
