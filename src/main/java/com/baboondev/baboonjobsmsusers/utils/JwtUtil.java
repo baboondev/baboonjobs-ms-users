24a28e2a39e9952c0669a742d3b250ef678f0983
msosa <marcossosa011@gmail.com>
2021-11-28 23:40:30 -0300

    bugfix: refactor jwt utils

--- src/main/java/com/baboondev/baboonjobsmsusers/utils/JwtUtil.java
+++ src/main/java/com/baboondev/baboonjobsmsusers/utils/JwtUtil.java
@@ -1,21 +1,27 @@
 package com.baboondev.baboonjobsmsusers.utils;
 
+import java.security.Key;
 import java.util.Date;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
 
 import com.baboondev.baboonjobsmsusers.models.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 public class JwtUtil {
-    private static final String JWT_SECRET = "JWT_SECRET";
-
     public static String generateToken(User user) {
+        String secret = "JWT_SECRET";
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secret);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, SignatureAlgorithm.HS256.getJcaName());
         return Jwts
                 .builder()
                 .claim("id", user.get_id())
                 .claim("role", user.getRole().getName())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 6000000))
-                .signWith(SignatureAlgorithm.HS512, JWT_SECRET.getBytes()).compact();
+                .signWith(signatureAlgorithm, signingKey).compact();
     }
 }
